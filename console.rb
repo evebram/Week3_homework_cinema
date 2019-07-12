b203a92dd5a9211a5b277bce973006146a5c427a
= <evebramley@me.com>
2019-07-12 13:56:26 +0100

    new tickets in console

--- console.rb
+++ console.rb
@@ -5,30 +5,49 @@ require_relative('models/customers')
 require('pry-byebug')
 
 film1 = Film.new({
-  'title' => 'Godzilla',
+  'title' => 'The Godfather',
+  'price' => 8
+  })
+film1 = Film.new({
+  'title' => 'Pretty Woman',
   'price' => 6
   })
 film1 = Film.new({
-  'title' => 'Godzilla',
-  'price' => 6
-  })
-film1 = Film.new({
-  'title' => 'Godzilla',
+  'title' => 'Kill Bill',
   'price' => 6
   })
 
 customer1 = Customer.new({
-  'name' => 'Bruno'
+  'name' => 'Bruno',
   'funds' => 20
   })
-customer1 = Customer.new({
-  'name' => 'Joseph'
+customer2 = Customer.new({
+  'name' => 'Joseph',
   'funds' => 30
   })
-customer1 = Customer.new({
-  'name' => 'Trish'
+customer3 = Customer.new({
+  'name' => 'Trish',
   'funds' => 15
   })
+
+ticket1 = Ticket.new({
+  'customer_id' => customer1.id,
+  'film_id' => film1.id
+  })
+ticket1 = Ticket.new({
+  'customer_id' => customer1.id,
+  'film_id' => film2.id
+  })
+ticket1 = Ticket.new({
+  'customer_id' => customer2.id,
+  'film_id' => film3.id
+  })
+ticket1 = Ticket.new({
+  'customer_id' => customer3.id,
+  'film_id' => film3.id
+  })
+
+
 
 
 binding.pry
