e4b5768bb17148aa07ac96ae7443d5808f66c208
= <evebramley@me.com>
2019-07-12 13:35:01 +0100

    set up console films

---
+++ console.rb
@@ -0,0 +1,21 @@
+require_relative('models/ticket')
+require_relative('models/films')
+require_relative('models/customers')
+
+require('pry-byebug')
+
+film1 = Film.new({
+  'title' => 'Godzilla',
+  'price' => 6
+  })
+film1 = Film.new({
+  'title' => 'Godzilla',
+  'price' => 6
+  })
+film1 = Film.new({
+  'title' => 'Godzilla',
+  'price' => 6
+  })
+
+binding.pry
+nil
